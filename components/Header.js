7796509cf78782696cb5aa5eebbeb73373efe0b5
mickey <damapongm@gmail.com>
2022-05-27 18:17:10 -0700

    jpg to png

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css';
 export default function Header() {
 	return (
 		<main className={styles.header}>
-			<img src='/assets/images/mickey.jpg' className={styles.dp} />
+			<img src='/assets/images/mickey.png' className={styles.dp} />
 
 			<h1 className={styles.name}>Mickey Damapong</h1>
 
