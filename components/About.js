4858c1a69026cb2194bad8f2f2f1f3d75bb34c07
mickey <damapongm@gmail.com>
2022-05-27 17:20:08 -0700

    bug fix 2

--- components/About.js
+++ components/About.js
@@ -17,37 +17,6 @@ export default function About() {
 				As a full-stack developer I can work with Next.js, React.js, React
 				Native, Express.js and MongoDB.
 			</p>
-			{/* <p>
-				I am writing technical contents on{' '}
-				<a href='https://techomoro.com' target='_blank' rel='noreferrer'>
-					Techomoro.com
-				</a>
-				,{' '}
-				<a href='https://dev.to/syamjayaraj' target='_blank' rel='noreferrer'>
-					Dev.to
-				</a>
-				,{' '}
-				<a
-					href='https://www.toolbox.com/user/about/SyamlalCM/'
-					target='_blank'
-					rel='noreferrer'>
-					Toolbox.com
-				</a>
-				,{' '}
-				<a
-					href='https://medium.com/@syamjayaraj'
-					target='_blank'
-					rel='noreferrer'>
-					Medium
-				</a>
-				, and{' '}
-				<a
-					href='https://www.quora.com/profile/Syamlal-CM'
-					target='_blank'
-					rel='noreferrer'>
-					Quora
-				</a>
-			</p> */}
 		</div>
 	);
 }
